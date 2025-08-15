152f97e3bcf6197cbe5572da6590df19d363f500
nessrine20 <nesshnainia@gmail.com>
2025-08-15 18:28:25 +0200

    clean project

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//imprtation des biblio
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//imprtation de routage
 var app = express();
+//creation de projet
 
-//// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,11 +23,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//malahou w ma3alayh
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+//erreur cote client
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -37,5 +41,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+//erreur cote serveur
 
 module.exports = app;
